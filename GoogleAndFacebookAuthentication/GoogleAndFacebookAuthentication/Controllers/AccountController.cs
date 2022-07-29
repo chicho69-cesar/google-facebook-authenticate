76686c0d53092fcbdea876fb3c862efd01373c8d
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-29 16:31:06 -0500

    Autenticacion con Facebook funcionando

--- GoogleAndFacebookAuthentication/GoogleAndFacebookAuthentication/Controllers/AccountController.cs
+++ GoogleAndFacebookAuthentication/GoogleAndFacebookAuthentication/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,28 @@ namespace GoogleAndFacebookAuthentication.Controllers {
 
         [Route("google-response")]
         public async Task<IActionResult> GoogleResponse() {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claims = result.Principal.Identities
+                .FirstOrDefault()
+                .Claims.Select(claim => new {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+
+            return Json(claims);
+        }
+
+        [Route("facebook-login")]
+        public IActionResult FacebookLogin() {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("FacebookResponse") };
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+        }
+
+        [Route("facebook-response")]
+        public async Task<IActionResult> FacebookResponse() {
             var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
             var claims = result.Principal.Identities
--- GoogleAndFacebookAuthentication/GoogleAndFacebookAuthentication/Program.cs
+++ GoogleAndFacebookAuthentication/GoogleAndFacebookAuthentication/Program.cs
@@ -1,3 +1,18 @@
+/*Para la autenticacion con google debemos de irnos a la pagina
+console.cloud.google.com, iniciar sesion y crear un proyecto nuevo,
+primero nos vamos a biblioteca y habilitamos la api de google, la cual
+se llama Google+ api, una vez habilitada nos vamos al apartado de 
+pantalla de consentimiento, aqui creamos un nuevo proyecto, lo
+seleccionamos y posteriormente nos vamos a credenciales, aqui creamos
+unas credenciales para nuestro proyecto, una vez hecho esto nos dara
+nuestro ClientId y nuestro ClientSecret para la autenticacion*/
+
+/*Para la autenticacion con facebook debemos de irnos a la pagina
+https://developers.facebook.com/, iniciar sesion con nuestra cuenta
+de facebook, despues creamos una aplicacion, configuramos el servicio,
+de inicio de sesion y nos daran nuestra AppId y AppSecret y ya 
+podemos llevar a cabo la autenticacion*/
+
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +23,16 @@ builder.Services.AddAuthentication(options => {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 })
     .AddCookie(options => {
-        options.LoginPath = "/account/google-login";
+        //options.LoginPath = "/account/google-login";
+        options.LoginPath = "/account/facebook-login";
     })
     .AddGoogle(options => {
         options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
         options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    })
+    .AddFacebook(options => {
+        options.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+        options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
     });
 
 var app = builder.Build();
