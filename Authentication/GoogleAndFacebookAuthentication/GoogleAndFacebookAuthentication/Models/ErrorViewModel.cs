4f1e3e1ea5afa7b38b7b7e02f6618bd2f83cb134
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-29 14:14:31 -0500

    Bug resolved

---
+++ Authentication/GoogleAndFacebookAuthentication/GoogleAndFacebookAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace GoogleAndFacebookAuthentication.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Authentication/README.md
@@ -0,0 +1,3 @@
+# Autenticacion con Google y Facebook en ASP.NET CORE MVC 6
+
+Autenticacion en ASP.NET CORE mediante login externos con google y con facebook
